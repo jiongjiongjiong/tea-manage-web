5432e8cc1979b09c592971173da7975a3f33dd02
zc <cong.2036@gmail.com>
2017-10-25 00:09:39 +0800

    添加静态文件

---
+++ src/main/java/com/zc/tea/controller/PageController.java
@@ -0,0 +1,24 @@
+package com.zc.tea.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author: zc
+ * @date: 2017/10/24
+ */
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page){
+        return page;
+    }
+}
